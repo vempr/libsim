97498418b0c497c0c55c7d93adac9efee1815d6d
Alex <alexnguyena6@gmail.com>
2025-05-06 18:21:45 +0200

    app.tsx

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,10 +5,8 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
 createInertiaApp({
-	title: (title) => `${title} - ${appName}`,
+	title: (title) => `${title} - libsim`,
 	resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
 	setup({ el, App, props }) {
 		const root = createRoot(el);
@@ -16,7 +14,7 @@ createInertiaApp({
 		root.render(<App {...props} />);
 	},
 	progress: {
-		color: '#4B5563',
+		color: 'oklch(0.45 0.1751 359.91)',
 	},
 });
 
