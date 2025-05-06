fc8a6801b44e7089fba357f3bb5af292617d367a
Alex <alexnguyena6@gmail.com>
2025-05-06 17:48:56 +0200

    logo and favicon

---
+++ resources/js/components/app-logo-icon.tsx
@@ -0,0 +1,7 @@
+export default function AppLogoIcon() {
+	return (
+		<div className="flex aspect-square size-8 items-center justify-center rounded-md">
+			<img src='/logo.jpg' className="size-8 rounded-sm" />
+		</div>
+	);
+}
--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,9 +1,9 @@
+import AppLogoIcon from "./app-logo-icon";
+
 export default function AppLogo() {
 	return (
 		<>
-			<div className="flex aspect-square size-8 items-center justify-center rounded-md">
-				<img src='/logo.jpg' className="size-8 rounded-sm" />
-			</div>
+			<AppLogoIcon />
 			<div className="ml-1 grid flex-1 text-left text-sm">
 				<span className="mb-0.5 truncate leading-none font-semibold">libsim</span>
 			</div>
