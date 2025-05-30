8cb8a74596069d27681cc87ca34a4aadcb0ada98
Alex <alexnguyena6@gmail.com>
2025-05-30 17:08:32 +0200

    ui: subtle pulsing during navigation loading

--- resources/js/layouts/app/app-sidebar-layout.tsx
+++ resources/js/layouts/app/app-sidebar-layout.tsx
@@ -3,15 +3,27 @@ import { AppShell } from '@/components/app-shell';
 import { AppSidebar } from '@/components/app-sidebar';
 import { AppSidebarHeader } from '@/components/app-sidebar-header';
 import { type BreadcrumbItem } from '@/types';
+import { Inertia } from '@inertiajs/inertia';
 import { type PropsWithChildren } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 export default function AppSidebarLayout({ children, breadcrumbs = [] }: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
+  const [transitioning, setTransitioning] = useState(false);
+
+  useEffect(() => {
+    Inertia.on('start', () => setTransitioning(true));
+    Inertia.on('finish', () => setTransitioning(false));
+  }, []);
+
+  const pageTransition = useMemo(() => (transitioning ? 'motion-safe:animate-pulse' : ''), [transitioning]);
+
   return (
     <AppShell variant="sidebar">
       <AppSidebar />
+
       <AppContent variant="sidebar">
         <AppSidebarHeader breadcrumbs={breadcrumbs} />
-        {children}
+        <div className={pageTransition}>{children}</div>
       </AppContent>
     </AppShell>
   );
