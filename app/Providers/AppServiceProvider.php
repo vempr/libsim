6246ef970b9c030df8cac8769c8f81b27dc9bcf3
Alex <alexnguyena6@gmail.com>
2025-07-01 22:23:44 +0200

    deploy: attempt 13

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider {
@@ -16,6 +19,18 @@ class AppServiceProvider extends ServiceProvider {
 	 * Bootstrap any application services.
 	 */
 	public function boot(): void {
-		//
+		$this->app[Kernel::class]->prependMiddleware(
+			function (Request $request, $next) {
+				$request->setTrustedProxies(
+					['*'], // Trust all proxies
+					Request::HEADER_X_FORWARDED_FOR |
+						Request::HEADER_X_FORWARDED_HOST |
+						Request::HEADER_X_FORWARDED_PORT |
+						Request::HEADER_X_FORWARDED_PROTO |
+						Request::HEADER_X_FORWARDED_AWS_ELB
+				);
+				return $next($request);
+			}
+		);
 	}
 }
