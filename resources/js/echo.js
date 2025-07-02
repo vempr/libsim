ef1beadb8199bb111c0c5f64c5e0808a2ede91f2
Alex <alexnguyena6@gmail.com>
2025-07-02 18:39:31 +0200

    deploy: attempt 19

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,7 +1,7 @@
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
-window.Pusher = new Pusher(import.meta.env.VITE_REVERB_APP_KEY, {cluster: import.meta.env.VITE_REVERB_APP_CLUSTER});
+window.Pusher = new Pusher(import.meta.env.VITE_APP_KEY, {cluster: import.meta.env.VITE_REVERB_APP_CLUSTER});
 
 window.Echo = new Echo({
   broadcaster: 'reverb',
